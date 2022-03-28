6d4bac8993b005d28005732d73ed588e0703cecb
Aria Tofighi <aria.tofighi1@gmail.com>
2022-03-28 16:35:12 -0700

    Added react app to test functions101 backend

---
+++ react-app/src/App.js
@@ -0,0 +1,38 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toons, setToons] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const res = await getToons();
+      const toons = await res.json();
+      setToons(toons);
+    })();
+  }, []);
+
+  const getToons = async () => {
+    return await fetch("/api/toons");
+  };
+
+  return (
+    <div className="App">
+      <table style={{ padding: "24px" }}>
+        {toons?.map((t) => (
+          <tr>
+            <td>{t?.id}</td>
+            <td>{t?.firstName}</td>
+            <td>{t?.lastName}</td>
+            <td>{t?.gender}</td>
+            <td>{t?.occupation}</td>
+            <td>{t?.votes}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default App;
